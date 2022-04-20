5deb2305288e17443955626e252fe7836ba9a1ed
Fábio Veiga <veigabfabio@hotmail.com>
2022-04-20 12:37:47 -0300

    20.04

--- src/title.js
+++ src/title.js
@@ -1,6 +1,7 @@
 function blink(){
     const slider = document.querySelector(".slider")
     var intervalId = setInterval(function() {
+        
         if (slider.classList.contains("show")){
             slider.classList.remove("show")
             slider.classList.add("hidden")
@@ -9,24 +10,53 @@ function blink(){
             slider.classList.add("show")
 
         }
-
       }, 630);
 }
 
-(function(){
+typeText("v37ga's repository")
+function typeText(text){
+    textUpper = text.toUpperCase();
     const titulo = document.querySelector(".titulo")
-    const string = "> V37GA'S REPOSITORY"
+    const string = "> "+textUpper;
     const usingSplit = string.split('');
+
     var i = 0
     finalString = ""
-
     var intervalo = setInterval(function(){
         finalString = finalString + usingSplit[i]
         titulo.textContent = finalString
         i++;
         if (i == usingSplit.length){
-            clearInterval(intervalo)
-            blink()
+            clearInterval(intervalo)      
         }
     }, 50)
-})()
+}
+
+blink();
+
+function eraseText(text){
+    
+    const titulo = document.querySelector(".titulo")
+    var currentText = titulo.textContent
+    const usingSplit = currentText.split('');
+
+    var tamanho = usingSplit.length
+
+    var intervalo = setInterval(function(){ 
+        var anterior = ""
+        var texto = ""
+        tamanho--
+        for (let i = 0; i <= tamanho;i++){  
+            
+            titulo.textContent = usingSplit[i]
+            texto = anterior+usingSplit[i]
+            anterior = texto
+    }
+    titulo.textContent = anterior
+        if (tamanho == 0){
+            clearInterval(intervalo)
+            typeText(text)
+        }
+
+    }, 25)
+}
