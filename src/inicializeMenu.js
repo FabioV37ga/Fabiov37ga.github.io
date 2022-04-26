76e094daa79bdc9e8af63b8d9f95e6bf1dfb6ce3
Fábio Veiga <veigabfabio@hotmail.com>
2022-04-26 13:42:21 -0300

    26.04

--- src/inicializeMenu.js
+++ src/inicializeMenu.js
@@ -1,48 +1,52 @@
-function showButton(){
+window.page = 1
+// 1 = menu
+// 2 = daycyclediscord
+// 3 = laptopcraft
+function showButton() {
     const button = document.querySelector(".iniciar")
     const pointers = document.querySelectorAll(".pointer")
 
-    button.addEventListener("click", () =>{
+    button.addEventListener("click", () => {
+        window.page++
         createBlock();
-        for (let i = 0; i <= pointers.length-1; i++){
-            pointers[i].remove() //!!!!!!!!!! isso vai mudar.
+        for (let i = 0; i <= pointers.length - 1; i++) {
+            pointers[i].remove()
         }
         console.log("button.click")
     })
 
-    button.addEventListener("animationend", function(){
+    button.addEventListener("animationend", function () {
         button.textContent = "INICIAR"
         button.style.padding = "10px 8.15px";
         button.style.marginLeft = "4px"
     })
 
     var i = 1
-    var intervalo = setInterval(function(){
-        if (i <= 6){
-            if (i == 1){
+    var intervalo = setInterval(function () {
+        if (i <= 6) {
+            if (i == 1) {
                 button.style.opacity = "100"
                 button.classList.add("open")
             }
-            if (pointers[0].style.opacity == "0"){
+            if (pointers[0].style.opacity == "0") {
                 pointers[0].style.opacity = "100"
                 pointers[1].style.opacity = "100"
-            }else{
+            } else {
                 pointers[0].style.opacity = "0"
                 pointers[1].style.opacity = "0"
             }
-        }else{
+        } else {
             clearInterval(intervalo)
-        }i++
-    },250);
+        } i++
+    }, 250);
 }
 
-window.page = 1
-function hideButton(){
+function hideButton() {
     const button = document.querySelector(".iniciar")
     button.textContent = ""
     button.classList.remove("open")
     button.classList.add("close")
-    button.addEventListener("animationend", ()=>{
+    button.addEventListener("animationend", () => {
         boxOpen();
         button.remove()
     })
--- src/title.js
+++ src/title.js
@@ -1,65 +1,65 @@
 var initState = 0;
-function init(){
-    if (initState == 0){
+function init() {
+    if (initState == 0) {
         showButton();
         initState = 1
     }
 }
 
-function blink(){
+function blink() {
     const slider = document.querySelector(".slider")
-    var intervalId = setInterval(function() {
-        
-        if (slider.style.opacity == 100){
+    var intervalId = setInterval(function () {
+
+        if (slider.style.opacity == 100) {
             slider.style.opacity = 0
-        }else{
+        } else {
             slider.style.opacity = 100
 
         }
-      }, 630);
+    }, 630);
 }
 
 typeText("v37ga's repository")
-function typeText(text){
+function typeText(text) {
     textUpper = text.toUpperCase();
     const titulo = document.querySelector(".titulo")
-    const string = "> "+textUpper;
-    const usingSplit = string.split('');
+    const string = "> " + textUpper;
+    const tituloSplit = string.split('');
 
     var i = 0
     finalString = ""
-    var intervalo = setInterval(function(){
-        finalString = finalString + usingSplit[i]
+    var intervalo = setInterval(function () {
+        finalString = finalString + tituloSplit[i]
         titulo.textContent = finalString
         i++;
-        if (i == usingSplit.length){
+        if (i == tituloSplit.length) {
             clearInterval(intervalo)
-            init()      
+            init()
         }
     }, 50)
-    
+
 }
 
 blink();
 
-function eraseText(text){
+function eraseText(text) {
     const titulo = document.querySelector(".titulo")
     var currentText = titulo.textContent
-    const usingSplit = currentText.split('');
+    const tituloSplit = currentText.split('');
 
-    var tamanho = usingSplit.length
-    var intervalo = setInterval(function(){ 
+    var tamanho = tituloSplit.length
+    var intervalo = setInterval(function () {
         var anterior = ""
         var texto = ""
         tamanho--
-        for (let i = 0; i <= tamanho;i++){  
-            
-            titulo.textContent = usingSplit[i]
-            texto = anterior+usingSplit[i]
+        for (let i = 0; i <= tamanho; i++) {
+
+            titulo.textContent = tituloSplit[i]
+            texto = anterior + tituloSplit[i]
             anterior = texto
-    }
-    titulo.textContent = anterior
-        if (tamanho == 0){
+        }
+        titulo.textContent = anterior
+        if (tamanho == 0) {
             clearInterval(intervalo)
             typeText(text)
         }
@@ -68,7 +68,7 @@ function eraseText(text){
 }
 
 const button = document.querySelector(".iniciar")
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
     eraseText("Day Cycle Discord")
     hideButton()
 })
