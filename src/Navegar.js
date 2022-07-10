f3e3cbb1839b997cf86353d40050a70f6dc814e6
Fábio Veiga <veigabfabio@hotmail.com>
2022-07-10 20:56:58 -0300

    10.07 - Janela.abre() adjustments

--- src/Navegar.js
+++ src/Navegar.js
@@ -48,8 +48,8 @@ class Janela {
                     const descricao = $("<div>", { class: "repositorio__descricao" }).insertAfter(".repositorio__imagem")
                     const botoes = $("<div>", { class: "repositorio__botoes" }).insertAfter(".repositorio__descricao")
                     {
-                        const botao_l = $("<button>", { class: "repositorio__botao" }).appendTo(".repositorio__botoes")
-                        const botao_r = $("<button>", { class: "repositorio__botao" }).appendTo(".repositorio__botoes")
+                        const botao_l = $("<button>", { class: "repositorio__botao", text: "DOWNLOAD" }).appendTo(".repositorio__botoes")
+                        const botao_r = $("<button>", { class: "repositorio__botao", text: "GITHUB" }).appendTo(".repositorio__botoes")
                     }
                 }
             }
@@ -60,7 +60,7 @@ class Janela {
         }
 
         // Armazena o elementos criados nos atributos da classe:
-        
+
         this.bloco_janela = document.querySelector(".bloco__janela");
         this.repositorio = document.querySelector(".repositorio");
         this.portas = document.querySelectorAll(".porta");
@@ -73,7 +73,7 @@ class Janela {
         this.abre()
     }
 
-    static abre(){
+    static abre() {
         // Largura da janela
         var largura_atual = 0;
         var largura_final = 630;
@@ -84,36 +84,41 @@ class Janela {
         var abertura = 0;
         // Loop com intervalo:
         var intervalo = setInterval(() => {
-            if (largura_atual < largura_final){
+            if (largura_atual < largura_final) {
                 // Incrementa largura_atual + 5 a cada execução até 630
                 largura_atual += 5
                 // Aplica a nova largura a cada execução, animando a largura.
                 this.repositorio.style.width = `${largura_atual}px`
-            }else{
+            } else {
                 // Quando a animação de largura termina, executa:
                 // Marca o repositorio como ligado
-                this.repositorio.classList.remove("off")
-                if (altura_atual < altura_final){
+                Janela.repositorio.classList.remove("off")
+                if (altura_atual < altura_final) {
                     // Incrementa + 4 na altura atual a cada execução até 652
                     altura_atual += 4
                     // Aplica a nova altura a cada execução, animando altura.
                     this.repositorio.style.height = `${altura_atual}px`
-                }else{
+                } else {
                     // Quando terminar de animar altura chama abre_portas()
                     abre_portas()
                 }
             }
-            
             // Essa função serve para animar as portas, revelando o conteudo abaixo delas.
-            function abre_portas(){
+            function abre_portas() {
+
                 // Enquanto não estiver aberto 100%...
-                if (abertura < 100){
+                if (abertura < 100) {
                     // Adiciona 0.5 por execução
-                    abertura+= 0.5;
+                    abertura += 0.5;
                     // Aplica o espaçamento na janela, abrindo-a.
                     Janela.portas[1].style.marginLeft = `${abertura}%`;
                     // console.log(Janela.portas)
-                }else{
+                } else {
+                    // ao fim da animação, remove o elemento.
+                    document.querySelector(".portas").remove()
+                    Janela.pointers[0].style.opacity = "100%"
+                    Janela.pointers[1].style.opacity = "100%"
+                    // termina o loop
                     clearInterval(intervalo)
                 }
             }
